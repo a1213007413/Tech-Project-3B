f7a95d17279a7e8210a336bda774dbeec295457f
Wei Ming Guo <weimingguo@usf.edu>
2020-09-26 12:39:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tech_Project_3B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] n = new int[25];
+               
+                 Console.WriteLine("There are 25 elements should be populated in the array.");
+                   Console.WriteLine("___________________________________________________");
+                Console.WriteLine("");
+                
+
+                for ( int i = 0; i < 25; i++)
+                {
+                    n[i] = i + 1;
+                
+                }
+                foreach (int j in n) 
+                {
+                    int i = j;
+                    Console.WriteLine("Element value = {0}", i, j);
+                }
+                Console.WriteLine("");
+                Console.WriteLine("___________________________________________________");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+                 
+            }
+            catch
+            {
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey(true);
+            }
+            
+
+        }
+    }
+}
